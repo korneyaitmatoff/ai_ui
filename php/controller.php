fab0c5c2da413e31555eae73726b87f7508fd673
Starunov Andrei <ASStarunov@sberbank.ru>
2024-03-11 23:48:23 +0300

    feature: added error logger

--- php/controller.php
+++ php/controller.php
@@ -1,8 +1,6 @@
 <?php
-
-ini_set("log_errors", 1);
-ini_set("error_log", __DIR__ . "/php-error.log");
-error_log( "Hello, errors!" );
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
 require __DIR__ . "/api/bootstrap.php";
 
